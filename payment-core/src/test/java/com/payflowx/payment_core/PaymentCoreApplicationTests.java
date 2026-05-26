055afafa8cc6ab044508b6675553ee9dbe3d4462
sarveshavhad <sarveshavhad03@gmail.com>
2026-05-27 00:11:39 +0530

    Project Setup

---
+++ payment-core/src/test/java/com/payflowx/payment_core/PaymentCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payflowx.payment_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
